00bebe8a6af2e2ad886c3c376e994df26bbdf38c
Lucas Bovolini <lbovolini94@gmail.com>
2020-12-30 16:19:05 -0300

    Adicionado busca de usuario por username

--- src/main/java/com/gitlab/lbovolini/todo/repository/UserRepository.java
+++ src/main/java/com/gitlab/lbovolini/todo/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.gitlab.lbovolini.todo.repository;
 import com.gitlab.lbovolini.todo.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends MongoRepository<User, String> {
+
+    Optional<User> findByUsername(String username);
 }
