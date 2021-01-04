011d94e277bad3cd48de3b833290a8ae508a40e6
Lucas Bovolini <lbovolini94@gmail.com>
2021-01-04 15:56:40 -0300

    Adicionado formato de requisicao e resposta

--- src/main/java/com/gitlab/lbovolini/todo/controller/PublicUserController.java
+++ src/main/java/com/gitlab/lbovolini/todo/controller/PublicUserController.java
@@ -6,6 +6,7 @@ import com.gitlab.lbovolini.todo.security.UserCredentials;
 import com.gitlab.lbovolini.todo.service.AuthenticationService;
 import com.gitlab.lbovolini.todo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,7 +14,9 @@ import javax.validation.Valid;
 
 
 @RestController
-@RequestMapping("/public/users")
+@RequestMapping(path = "/public/users",
+        consumes = MediaType.APPLICATION_JSON_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE)
 public class PublicUserController {
 
     private final AuthenticationService authenticationService;
