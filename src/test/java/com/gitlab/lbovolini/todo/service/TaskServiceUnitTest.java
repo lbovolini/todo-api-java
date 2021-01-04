a5a892e7e1ff31973d73e8e986709d3519b89fcd
Lucas Bovolini <lbovolini94@gmail.com>
2021-01-04 14:34:22 -0300

    Limpeza de codigo

--- src/test/java/com/gitlab/lbovolini/todo/service/TaskServiceUnitTest.java
+++ src/test/java/com/gitlab/lbovolini/todo/service/TaskServiceUnitTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.dao.DuplicateKeyException;
 
 import java.time.ZonedDateTime;
 import java.util.List;
