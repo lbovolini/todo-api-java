5b553ea225388ae2f68e95357f485f3c13569c8d
Lucas Bovolini <lbovolini94@gmail.com>
2021-01-06 10:01:16 -0300

    Adicionado controller da lista de afazeres

--- src/main/java/com/gitlab/lbovolini/todo/todo/api/TodoController.java
+++ src/main/java/com/gitlab/lbovolini/todo/todo/api/TodoController.java
@@ -1,7 +1,7 @@
 package com.gitlab.lbovolini.todo.todo.api;
 
-import com.gitlab.lbovolini.todo.controller.CrudController;
-import com.gitlab.lbovolini.todo.todo.TodoService;
+import com.gitlab.lbovolini.todo.common.CrudController;
+import com.gitlab.lbovolini.todo.todo.service.TodoService;
 import com.gitlab.lbovolini.todo.todo.model.Todo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -13,10 +13,6 @@ import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
-/**
- * Consome: MediaType.APPLICATION_JSON
- * Produz: MediaType.APPLICATION_JSON
- */
 @RestController
 @RequestMapping(path = "api/v1/todos",
         consumes = MediaType.APPLICATION_JSON_VALUE,
@@ -30,11 +26,6 @@ public class TodoController implements CrudController<Todo> {
         this.todoService = todoService;
     }
 
-    /**
-     * Remove a tarefa com o id informado.
-     * @param id
-     * @return Retorna ResponseEntity com HttpStatus 204.
-     */
     @Override
     @DeleteMapping("{id}")
     public ResponseEntity<?> delete(@PathVariable String id) {
@@ -43,11 +34,6 @@ public class TodoController implements CrudController<Todo> {
         return ResponseEntity.noContent().build();
     }
 
-    /**
-     * Realiza a busca da tarefa pelo id informado.
-     * @param id
-     * @return Retorna ResponseEntity com a tarefa e com HttpStatus 200, ou HttpStatus 404 se a tarefa com o id informado não for encontrada.
-     */
     @Override
     @GetMapping("{id}")
     public ResponseEntity<Todo> findById(@PathVariable String id) {
@@ -59,10 +45,6 @@ public class TodoController implements CrudController<Todo> {
                 .orElse(ResponseEntity.notFound().build());
     }
 
-    /**
-     * Realiza a busca de todas as tarefas.
-     * @return Retorna ResponseEntity com a lista de todas as tarefas e com HttpStatus 200.
-     */
     @Override
     @GetMapping
     public ResponseEntity<List<Todo>> findAll() {
@@ -72,20 +54,12 @@ public class TodoController implements CrudController<Todo> {
     }
 
     @Override
-    public ResponseEntity<Todo> save(Todo todo) {
-        return null;
+    public ResponseEntity<Todo> save(@Valid @RequestBody Todo todo) {
+        Todo savedTodo = todoService.save(todo);
+
+        return ResponseEntity.ok(savedTodo);
     }
 
-    @Override
-    public ResponseEntity<?> update(Todo todo) {
-        return null;
-    }
-
-    /**
-     * Salva a tarefa na base de dados.
-     * @param task
-     * @return Retorna ResponseEntity com a tarefa salva e com HttpStatus 200, ou HttpStatus 409 se a tarefa informada já existe na base de dados.
-     */
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<Todo> save(@Valid @RequestPart Todo todo, @RequestParam MultipartFile attachment) {
         Todo savedTodo = todoService.save(todo, attachment);
@@ -93,11 +67,16 @@ public class TodoController implements CrudController<Todo> {
         return ResponseEntity.ok(savedTodo);
     }
 
-    /**
-     * Atualiza a tarefa na base de dados.
-     * @param task
-     * @return Retorna ResponseEntity com a tarefa atualizada e com HttpStatus 200, ou HttpStatus 404 se a tarefa com o id informado não for encontrada.
-     */
+    @Override
+    public ResponseEntity<Todo> update(@Valid @RequestBody Todo todo) {
+        Optional<Todo> todoOptional = todoService.update(todo);
+
+        return todoOptional.stream()
+                .map(ResponseEntity::ok)
+                .findFirst()
+                .orElse(ResponseEntity.notFound().build());
+    }
+
     @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<Todo> update(@Valid @RequestPart Todo todo, @RequestParam MultipartFile attachment) {
         Optional<Todo> todoOptional = todoService.update(todo, attachment);
