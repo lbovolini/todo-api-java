7db6ea38c0982fe9c443f85916a6eca1e930956f
Lucas Bovolini <lbovolini94@gmail.com>
2021-01-06 09:39:42 -0300

    Corrigido formato de data e tempo para o tipo ZonedDateTime durante a serializacao

---
+++ src/main/java/com/gitlab/lbovolini/todo/configuration/parser/JacksonConfig.java
@@ -0,0 +1,36 @@
+package com.gitlab.lbovolini.todo.configuration.parser;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Configuration
+public class JacksonConfig {
+
+    private final String dateTimeFormat;
+
+    @Autowired
+    public JacksonConfig(@Value("${date.time.format:yyyy-MM-dd'T'HH:mm:ss.SSSSSSSSSxxx}") String dateTimeFormat) {
+        this.dateTimeFormat = dateTimeFormat;
+    }
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+        return jacksonObjectMapperBuilder ->
+                jacksonObjectMapperBuilder.serializerByType(ZonedDateTime.class, new JsonSerializer<ZonedDateTime>() {
+                    @Override
+                    public void serialize(ZonedDateTime zonedDateTime, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+                        jsonGenerator.writeString(DateTimeFormatter.ofPattern(dateTimeFormat).format(zonedDateTime));
+                    }
+        });
+    }
+}
